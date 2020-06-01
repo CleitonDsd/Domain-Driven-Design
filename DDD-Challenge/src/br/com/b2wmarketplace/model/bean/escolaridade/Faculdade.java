5c37b7092b4b5c659c510ada6e15167ed4ea7bde
Cleiton Dias <s.diascleiton@gmail.com>
2020-05-31 23:12:45 -0300

    Formação do Candidato

--- DDD-Challenge/src/br/com/b2wmarketplace/model/bean/escolaridade/Faculdade.java
+++
@@ -1,8 +0,0 @@
-package br.com.bw2marketplace.model.bean.escolaridade;
-
-public class Faculdade extends InformacoesAcademicas {
-
-	private long idFaculdade;
-	private String nomeFaculdade;
-	private String nivelFormacao;
-}
--- DDD-Challenge/src/br/com/b2wmarketplace/model/bean/escolaridade/InformacoesAcademicas.java
+++ DDD-Challenge/src/br/com/b2wmarketplace/model/bean/escolaridade/InformacoesAcademicas.java
@@ -1,10 +1,57 @@
-package br.com.bw2marketplace.model.bean.escolaridade;
+package br.com.b2wmarketplace.model.bean.escolaridade;
 
 public class InformacoesAcademicas {
 
 	private String periodo;
 	private boolean cursando;
 	private String dataInicio;
-	private String dataConclusao;
 	private String previsaoTermino;
+
+	public String toString() {
+		return "\n Período: " + periodo + "\n Cursando: " + cursando + "\n Data de início: " + dataInicio
+				+ "\n Previsão de termino: " + previsaoTermino;
+	}
+
+	public InformacoesAcademicas() {
+
+	}
+
+	public InformacoesAcademicas(String periodo, boolean cursando, String dataInicio, String previsaoTermino) {
+		this.periodo = periodo;
+		this.cursando = cursando;
+		this.dataInicio = dataInicio;
+		this.previsaoTermino = previsaoTermino;
+	}
+
+	public String getPeriodo() {
+		return periodo;
+	}
+
+	public void setPeriodo(String periodo) {
+		this.periodo = periodo;
+	}
+
+	public boolean isCursando() {
+		return cursando;
+	}
+
+	public void setCursando(boolean cursando) {
+		this.cursando = cursando;
+	}
+
+	public String getDataInicio() {
+		return dataInicio;
+	}
+
+	public void setDataInicio(String dataInicio) {
+		this.dataInicio = dataInicio;
+	}
+
+	public String getPrevisaoTermino() {
+		return previsaoTermino;
+	}
+
+	public void setPrevisaoTermino(String previsaoTermino) {
+		this.previsaoTermino = previsaoTermino;
+	}
 }
