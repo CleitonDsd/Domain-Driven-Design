dd9a55ab0a2b0263b3621f191c8900bc92acc07a
Cleiton Dias <s.diascleiton@gmail.com>
2020-05-31 22:57:09 -0300

    Classe de produto, onde tem toda a lógica de calculo de juros e descontos

--- NAC2-Farmacia/src/br/com/brainfarma/model/bean/produto/Produto.java
+++ NAC2-Farmacia/src/br/com/brainfarma/model/bean/produto/Produto.java
@@ -24,16 +24,86 @@ public class Produto { // abstract
 		this.tipoProduto = tipoProduto;
 	}
 
-	// Imprimir produto
-	@Override
-	public String toString() {
-		return "n= = = = = = = = = = = = " + "\nNome: " + nome + "\nMarca: " + marca + "\nTipo do Produto: "
-				+ tipoProduto + "\nValor Unitário(R$): " + valorUnitario + "\n= = = = = = = = = = = = ";
+	public double calcularValorTotal(double valorUnitario, int quantidade) {
+
+		this.quantidade = quantidade;
+		this.valorUnitario = valorUnitario;
+		return valorUnitario * quantidade;
+
 	}
-	
-	
-	
-	
+
+	public void calcularDesconto(double valorTotal) {
+
+		if (valorTotal >= 250) {
+			System.out.println("Desconto de 10%");
+			double desconto = (valorTotal * 10) / 100;
+			System.out.println("Desconto: " + desconto);
+			valorTotal -= desconto;
+			System.out.println("Valor Total(R$): " + valorTotal);
+
+		} else if (quantidade >= 5) {
+			System.out.println("Desconto de 25%");
+			double desconto = (valorTotal * 25) / 100;
+			System.out.println("Desconto: " + desconto);
+			valorTotal -= desconto;
+			System.out.println("Valor Total(R$): " + valorTotal);
+		}
+
+		System.out.println("Valor Total (R$): " + valorTotal);
+	}
+
+	public void calcularJuros(double valorTotal, int quantidadeParcelas) {
+
+		double juros;
+		double valorParcelas;
+		double valorJuros;
+		if (quantidadeParcelas > 3) {
+			if (valorTotal <= 100) {
+				// calcular 5% de juros
+				juros = (valorTotal * 1.05);
+				valorJuros = juros - valorTotal;
+				valorTotal += valorJuros;
+				valorParcelas = valorTotal / quantidadeParcelas;
+				System.out.println("\n========== NFE ============\n");
+				System.out.println("Juros 5% | (R$)" + valorJuros);
+				System.out.println("Valor das Parcelas (R$): " + valorParcelas);
+				System.out.println("Valor Total (R$): " + valorTotal);
+
+			} else if (valorTotal < 250) {
+				// calcular 7% de juros
+				juros = (valorTotal * 1.15);
+				valorJuros = juros - valorTotal;
+				valorTotal += valorJuros;
+				valorParcelas = valorTotal / quantidadeParcelas;
+				System.out.println("\n========== NFE ============\n");
+				System.out.println("Juros 7% | (R$)" + valorJuros);
+				System.out.println("Valor das Parcelas (R$): " + valorParcelas);
+				System.out.println("Valor Total (R$): " + valorTotal);
+
+			} else if (valorTotal >= 250) {
+				// calcular 10% de juros
+				juros = (valorTotal * 1.10);
+				valorJuros = juros - valorTotal;
+				valorTotal += valorJuros;
+				valorParcelas = valorTotal / quantidadeParcelas;
+
+				System.out.println("\n========== NFE ============\n");
+				/* System.out.printf("Juros 10% | (R$) %.2f %n", juros); */
+				System.out.println("Juros 10% | (R$)" + valorJuros);
+				System.out.println("Valor das Parcelas (R$): " + valorParcelas);
+				System.out.println("Valor Total (R$): " + valorTotal);
+
+			}
+		}
+	}
+
+	public void cupomFiscalCartao(int quantidadeParcelas, double valorTotal) {
+
+		double valorPagoPorParcela = valorTotal / quantidadeParcelas;
+		System.out.println("Quantidade de Parcelas: " + quantidadeParcelas + "x");
+		System.out.printf("Valor das Parcelas(R$): %.2f %n", valorPagoPorParcela);
+		System.out.println("Valor Total: " + valorTotal);
+	}
 
 	public long getCodigoProduto() {
 		return codigoProduto;
