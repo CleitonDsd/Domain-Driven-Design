537b318705a2ad0413db0783cbd48d4113cdfb91
Cleiton Dias <s.diascleiton@gmail.com>
2020-07-09 16:41:09 -0300

    Metódos de calculação dos juros

---
+++ ExerciciosFerias_CalculadoraFinanceira/src/br/com/fiap/tds/bean/CalculadoraFinanceira.java
@@ -0,0 +1,20 @@
+package br.com.fiap.tds.bean;
+
+public class CalculadoraFinanceira {
+
+	public CalculadoraFinanceira() {
+
+	}
+
+	public double calcularJurosSimples(double capital, double taxaJuros, double periodo) {
+		double montante = capital * (1 + taxaJuros * periodo);
+		return montante;
+	}
+
+	public double calcularJurosCompostos(double capital, double taxaJuros, double periodo) {
+
+		double montante = capital * (1 + taxaJuros);
+		return Math.pow(montante, periodo);
+
+	}
+}
